c85f211303979846215e871e1bdcbf89d46b2cde
AdalheidurM <adm7@hi.is>
2023-03-01 16:22:22 +0000

    Uppfæra verkefni

--- Undirliggjandi/verkefni201-adal.R
+++ Undirliggjandi/verkefni201-adal.R
@@ -9,18 +9,14 @@ library(readr) ## fyrir að lesa inn gögn
 
 ##------------------------------------Lesa inn nýju gögnin----------------
 gagnarammi <- read_delim("DATA_SAL138F_IRT_Skulason.2022_Vidhorf.Til.Namsmats_v28n3404.binay.csv", 
-                   delim = ";", escape_double = FALSE, trim_ws = TRUE)
+                   delim = ";", escape_double = FALSE, trim_ws = TRUE, na = c("-999"))
 
 bakgrunnur <- read_delim("DATA_SAL138F_IRT_Skulason.2022_Vidhorf.Til.Namsmats_BAKGRUNNSBR.csv", 
-                         delim = ";", escape_double = FALSE, trim_ws = TRUE)
+                         delim = ";", escape_double = FALSE, trim_ws = TRUE, na = c("-999"))
 
 ## Snúa við breytum
 gagnarammi$sp_a02 = car::recode(gagnarammi$sp_a02, "0=1; 1=0")
-gagnarammi$sp_a03 = car::recode(gagnarammi$sp_a02, "0=1; 1=0")
-
-#-------------------------------------Taka burt -999--------------------
-gagnarammi[gagnarammi == -999] <- NA
-bakgrunnur[bakgrunnur == -999] <- NA
+gagnarammi$sp_a03 = car::recode(gagnarammi$sp_a03, "0=1; 1=0")
 
 
 #---------------------------------Filter----------------------------
@@ -40,7 +36,7 @@ UtanHBS <- filter(gagnarammi, Hof_land == 2) # Utan höfuðborgarsvæðis
 # Kennarar  vs foreldrar
 VidhorfKennara <- filter(gagnarammi,hopur == 2)
 VidhorfForeldra <- filter(gagnarammi, hopur == 1)
-VidhorfForeldra <- dplyr::select(VidhorfNemenda, !c(sp_a10, sp_b13, sp_b14)) #!spr fyrir kennara
+VidhorfForeldra <- dplyr::select(VidhorfForeldra, !c(sp_a10, sp_b13, sp_b14)) #!spr fyrir kennara
 
 #Starfsaldur
 "11lengur" <- filter(VidhorfKennara, Strf_ald == 3)
@@ -60,29 +56,22 @@ upplys_mat <- select(gagnarammi, c(sp_a01, sp_a02, sp_a03, sp_a04, sp_a05, sp_a0
 lykilhaefni <- select(gagnarammi, c(sp_c01, sp_c02, sp_c03, sp_c04))
 
 
-#-------------------Taka burt raðir með fleiri en 4 N/A gildum í spurningum-------------------------
-NAgildi <- rowSums(is.na(VidhorfKennara)) >= 4
-tilbuinnGogn <- gagnarammi[!NAgildi, ]
+#-------------------Taka burt raðir þá sem svöruðu engu------------------------
+NAgildi <- rowSums(is.na(VidhorfKennara[,-1:-8])) >= 30
+tilbuinnGogn <- VidhorfKennara[!NAgildi, ]
 sum(is.na(tilbuinnGogn))
 
 #----------------------------------------Tilreikningur----------------------------------------
 imputationgogn <- mice(tilbuinnGogn, m=3, method="pmm", maxit = 3)
 imputationgogn <- complete(imputationgogn, 3)
 sum(is.na(imputationgogn))
+
 describe(imputationgogn)
-
-
-##---------------------------------Lýsandi tölfræði--------------------------
-
-describe(VidhorfForeldra)
-describe(VidhorfKennara)
-
-psych::alpha(VidhorfForeldra)
-psych::alpha(VidhorfKennara)
+psych::alpha(imputationgogn)
 
 
 #----------------------------------------Svarferlalíkan----------------------------------------
-Likan <- mirt(imputationgogn, itemtype = "graded")
+Likan <- mirt(imputationgogn[,-1:-8], itemtype = "graded")
 coef(Likan, simplify = T)
 summary(Likan)
 itemfit(Likan)
