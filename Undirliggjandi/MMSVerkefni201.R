990ffa4bdf8149bc56388c1c7b21782c15a4699f
ioi8 <70318969+ioi8@users.noreply.github.com>
2023-03-01 13:15:43 +0000

    Verkefni201MMS

---
+++ Undirliggjandi/MMSVerkefni201.R
@@ -0,0 +1,112 @@
+"Verkefni 201
+A: Spurningar um  mikilvægi upplýsigna og um framkvæmd mats í skólum 10 atriði
+B: Spurningar um hlutverk námsmats 14  atriði
+C: Spurningar um  mat tengt lykilhæfni menntunar í aðalnámsskrá 4 atriði
+
+Tveim breytum var snúið við:
+- Mér finnst að skólar ættu einir að sjá um mat á námsárangri nemenda sinna.
+- Mér finnst hlutur námsmats í skólastarfi of mikill.
+
+tilgátur:
+  - Hvernig er viðhorf smærri skóla til náms vs. stærri
+- Viðhorf kennara til náms m.t.t. starfsaldur
+- Viðhorf til náms eftir landhluta (Norðvesturkjör, Norðaustur, Suðurkjördæmi, HBS)"
+
+rm(list = ls()) 
+graphics.off() 
+cat("\014") 
+
+#----------------------------------------Pakkar----------------------------------------
+library(mirt)
+library(tidyverse)
+library(parallel)
+library(readr)
+library(mice)
+library(mirt)
+library(tidyverse)
+
+
+#----------------------------------------Gögn----------------------------------------
+gagnarammi <- read_delim("./Documents/GitHub/Megindleg.github.io/Undirliggjandi/DATA_SAL138F_IRT_Skulason.2022_Vidhorf.Til.Namsmats_v28n3404.binay.csv", 
+                   delim = ";", escape_double = FALSE, trim_ws = TRUE, na = c("-999"))
+
+#----------------------------------------Breytur----------------------------------------
+#ATH AÐEINS KEYRA EINA BAKGRUNSBREYTU Í SENN!
+
+##---------------------------------------Stærð skóla------------------------------------
+
+StorSkoli <- filter(gagnarammi, stard_sk == 51)
+litillSkoli <- filter(gagnarammi,stard_sk == 1 & 11 & 21 & 31 & 41)
+##----------------------------------------Staðsetning------------------------------------
+HBS <- filter(gagnarammi, Ladhl == 1)
+NordVest <- filter(gagnarammi, Ladhl == 3)
+NordAust <- filter(gagnarammi, Ladhl == 6)
+Sud <- filter(gagnarammi, Ladhl == 8)
+UtanHBS <- filter(gagnarammi, Hof_land == 2)
+
+##----------------------------------------Viðhorf------------------------------------------
+VidhorfKennara <- filter(gagnarammi,hopur == 2)
+VidhorfNemenda <- filter(gagnarammi, hopur == 1)
+VidhorfNemenda <- dplyr::select(VidhorfNemenda, !c(sp_a10, sp_b05, sp_b14)) #!spr fyrir kennara
+
+###----------------------------------------Starfsaldur--------------------------------------
+"11lengur" <- filter(VidhorfKennara, Strf_ald == 3)
+"6til10" <- filter(VidhorfKennara,Strf_ald == 2)
+"0til5" <- filter(VidhorfKennara,Strf_ald == 1)
+
+
+#----------------------------------------Veljum spurningar-----------------------------------
+##---Spurningar um  mikilvægi upplýsigna og um framkvæmd mats í skólum-----------------------
+hlutverk_namsmats <- dplyr::select(gagnarammi, c(sp_b01, #hve vel nemendur eru búnir undir daglegt líf og starf á fullorðinsárum
+                                                 sp_b02, #hve vel nemendur eru búnir undir nám í framhaldsskóla.
+                                                 sp_b03, #hvort nemendur þurfi á sérstökum stuðningi að halda.
+                                                 sp_b04, #hvort nemendur þurfi fleiri áskoranir í námi.
+                                                 sp_b05, #námsframvindu nemenda milli skólaára.
+                                                 sp_b06, #skilning nemenda á sjálfbærni.
+                                                 sp_b07, #styrk- og veikleika nemenda við upphaf grunnskóla.
+                                                 sp_b08, #stöðu nemenda í einstökum námsgreinum.
+                                                 sp_b09, #stöðu nemenda í því námsefni sem unnið er með í kennslu hverju sinni.
+                                                 sp_b10, #stöðu nemenda miðað við jafnaldra.
+                                                 sp_b11, #stöðu nemenda út frá viðmiðum aðalnámskrár.
+                                                 sp_b12, #stöðu nemenda þegar kennari eða forsjáraðili telur þörf á.
+                                                 sp_b13, #námsframvindu nemenda á sama skólaári.
+                                                 sp_b14  #hve vel nemendur eru búnir undir nám á næsta stigi grunnskólans
+                                                 ))
+##---------------------------------Spurningar um hlutverk námsmats------------------------------
+upplys_mat <- dplyr::select(gagnarammi, c(sp_a01, #Ég tel mikilvægt að utanaðkomandi aðili meti námsárangur nemenda til viðbótar við námsmat skólans.
+                                          sp_a02, #Mér finnst að skólar ættu einir að sjá um mat á námsárangri nemenda sinna.
+                                          sp_a03, #Mér finnst hlutur námsmats í skólastarfi of mikill.
+                                          sp_a04, #Mér finnst námsmat nauðsynlegur hluti skólastarfs.
+                                          sp_a05, #Mikilvægt er að framhaldsskólar sem það kjósa geti notað eigin próf við inntöku nýnema.
+                                          sp_a06, #Mikilvægt er að kennarar í öllum skólum landsins hafi aðgang að sömu prófum og matstækjum.
+                                          sp_a07, #Mikilvægt er að til séu upplýsingar um stöðu einstakra skóla.
+                                          sp_a08, #Mikilvægt er að til séu upplýsingar um stöðu skóla í einstökum landshlutum eða sveitarfélögum.
+                                          sp_a09, #Mikilvægt er að til séu upplýsingar um stöðu skólakerfisins almennt.
+                                          sp_a10  #Ég á auðvelt með að meta frammistöðu nemenda samkvæmt viðmiðum aðalnámskrár.
+                                          ))
+##-----------------------------Spurningar um  mat tengt lykilhæfni manntunar í aðalnámsskrá-------
+lykilhaefni <- dplyr::select(gagnarammi, c(sp_c01, #velferð nemenda í skóla.
+                                           sp_c02, #vinnubrögð nemenda og námstækni.
+                                           sp_c03, #virkni nemenda í lýðræði og jafnrétti.
+                                           sp_c04  #getu nemenda til sköpunar.
+                                           ))
+
+#-------------------Taka burt raðir með fleiri en 4 N/A gildum í spurningum-------------------------
+NAgildi <- rowSums(is.na(gagnarammiTilbuin)) >= 4
+tilbuinnGogn <- gagnarammiTilbuin[!NAgildi, ]
+sum(is.na(tilbuinnGogn))
+
+#----------------------------------------Tilreikningur----------------------------------------
+imputationgogn <- mice(tilbuinnGogn, m=3, method="pmm", maxit = 3)
+imputationgogn <- complete(imputationgogn, 3)
+sum(is.na(imputationgogn))
+describe(imputationgogn)
+
+#----------------------------------------Svarferlalíkan----------------------------------------
+Likan <- mirt(imputationgogn, itemtype = "graded")
+coef(Likan, simplify = T)
+summary(Likan)
+itemfit(Likan)
+itemplot(Likan, 3)
+itemplot(Likan, 3, type = 'threshold', )
+
--- Undirliggjandi/verkefni201_is.R
+++ Undirliggjandi/verkefni201_is.R
@@ -4,6 +4,10 @@ cat("\014")
 
 library(mirt)
 library(tidyverse)
+library(parallel)
+library(MASS)
+library(mice)
+library(tictoc)
 
 #Verkefni 201 - Svarferlalíkan fyrir RAASI gögn
 gagnarammi <- read.delim("./Documents/GitHub/Megindleg.github.io/Undirliggjandi/gogninn.dat", header=F, row.names = 1)
@@ -14,47 +18,49 @@ colnames(gagnarammi) <- c("kyn", "ald_hop", "skolstig", "JS_01", "SS_02", "AH_03
                        "TA_22", "TA_23", "AH_24", "AH_25", "TA_26", "TA_27",
                        "TA_28", "TA_29", "TA_30", "TA_31", "AH_32")
 
-#sum(is.na(gagnarammi)) #ATH á N/A-gildum
-#rowSums(gagnarammi) #Telja hversu mörg gildi ery í hverri röð
-#describe(gagnarammi) #Lýsandi tölfræði
+sum(is.na(gagnarammi)) #ATH á N/A-gildum
+rowSums(gagnarammi) #Telja hversu mörg gildi ery í hverri röð
+describe(gagnarammi) #Lýsandi tölfræði
 
 
 #-----Svarferlalíkan-----
 #ATH AÐEINS KEYRA EINA BAKGRUNSBREYTU Í SENN!
 #Skoða kynjahlutföll 
-filter(gagnarammi, kyn == 1) #Filter by KK
-filter(gagnarammi, kyn == 2) #Filter by KVK
+gagnarammi <- filter(gagnarammi, kyn == 1) #Filter by KK
+gagnarammi <- filter(gagnarammi, kyn == 2) #Filter by KVK
 
 #Skoða skólahlutföll
-filter(gagnarammi, skolastig == 1) #Yngrastig
-filter(gagnarammi, skolastig == 2) #Eldrastig
+gagnarammi<- filter(gagnarammi, skolstig == 1) #Yngrastig
+gagnarammi<- filter(gagnarammi, skolstig == 2) #Eldrastig
 
 #Skoða yngra skólastig með yngri og eldri 
-filter(gagnarammi, ald_hop == 11) #Yngri á yngrastigi
-filter(gagnarammi, ald_hop == 12) #Eldri á yngrastigi
+gagnarammi<- filter(gagnarammi, ald_hop == 11) #Yngri á yngrastigi
+gagnarammi<- filter(gagnarammi, ald_hop == 12) #Eldri á yngrastigi
 
 #Skoða eldra skólastig með yngri og eldri 
-filter(gagnarammi, ald_hop == 21) #Yngri á eldrastigi
-filter(gagnarammi, ald_hop == 22) #Eldri á eldrastigi
+gagnarammi<- filter(gagnarammi, ald_hop == 21) #Yngri á eldrastigi
+gagnarammi<- filter(gagnarammi, ald_hop == 22) #Eldri á eldrastigi
 
-
-heildarskor <- select(gagnarammi, !c("kyn","ald_hop", "skolstig"))
-jakvedsjalfsimynd <- select(gagnarammi, c("JS_01", "JS_04", "JS_06", "JS_08", "JS_16", "JS_19"))
-skapstjorn <- select(gagnarammi, c("SS_02", "SS_05", "SS_10", "SS_11", "SS_12", "SS_15"))
-tilfinningaalag <- select(gagnarammi, c("TA_18", "TA_20", "TA_22", "TA_23", "TA_26", "TA_27", "TA_28", "TA_29", "TA_30", "TA_31"))
-andfelagsleghegdun <- select(gagnarammi, c("AH_03", "AH_13", "AH_14", "AH_17", "AH_21", "AH_24", "AH_25", "AH_32"))
-
+#---Tilreikningur----
+sum(is.na(gagnarammi)) #ATH á N/A-gildum
+set.seed(7)
+m <- mice(gagnarammi, method="pmm", m = 5 )
+m <- m$data
+sum(is.na(gagnarammi)) #ATH á N/A-gildum
 
 #----Svarferlalíkan fyrir Jákvæðasjálfsímynd ------
-JS.likan <- mirt(jakvedsjalfsimynd, itemtype = "graded", method = "MCEM", iter = 1000)
+jakvedsjalfsimynd <- gagnarammi[c("JS_01", "JS_04", "JS_06", "JS_08", "JS_16", "JS_19")]
+
+JS.likan <- mirt(jakvedsjalfsimynd, itemtype = "graded")
 coef(JS.likan, simplify = T)
 summary(JS.likan)
 itemfit(JS.likan)
 itemplot(JS.likan, 3)
-itemplot(JS.likan, 3, type = 'threshold')
+itemplot(JS.likan, 3, type = 'threshold', )
 
 #----Svarferlalíkan fyrir Skapstjórn ------
-SS.likan <- mirt(skapstjorn, itemtype = "graded", method = "MCEM", iter = 1000)
+skapstjorn <- gagnarammi[c("SS_02", "SS_05", "SS_10", "SS_11", "SS_12", "SS_15")]
+SS.likan <- mirt(skapstjorn, itemtype = "graded")
 coef(SS.likan, simplify = T)
 summary(SS.likan)
 itemfit(SS.likan)
@@ -62,7 +68,8 @@ itemplot(SS.likan, 3)
 itemplot(SS.likan, 3, type = 'threshold')
 
 #----Svarferlalíkan fyrir Tilfinninga álag ------
-TA.likan <- mirt(tilfinningaalag, itemtype = "graded", method = "MCEM", iter = 1000)
+tilfinningaalag <- gagnarammi[c("TA_18", "TA_20", "TA_22", "TA_23", "TA_26", "TA_27", "TA_28", "TA_29", "TA_30", "TA_31")]
+TA.likan <- mirt(tilfinningaalag, itemtype = "graded")
 coef(TA.likan, simplify = T)
 summary(TA.likan)
 itemfit(TA.likan)
@@ -70,7 +77,8 @@ itemplot(TA.likan, 3)
 itemplot(TA.likan, 3, type = 'threshold')
 
 #----Svarferlalíkan fyrir Andfélagslega hegðun ------
-AH.likan <- mirt(andfelagsleghegdun, itemtype = "graded", method = "MCEM", iter = 1000)
+andfelagsleghegdun <- gagnarammi[c("AH_03", "AH_13", "AH_14", "AH_17", "AH_21", "AH_24", "AH_25", "AH_32")]
+AH.likan <- mirt(andfelagsleghegdun, itemtype = "graded")
 coef(AH.likan, simplify = T)
 summary(AH.likan)
 itemfit(AH.likan)
@@ -78,13 +86,25 @@ itemplot(AH.likan, 3)
 itemplot(AH.likan, 3, type = 'threshold')
 
 #----Svarferlalíkan fyrir RAASI ------
-Heild.likan <- mirt(heildarskor, itemtype = "graded", method = "MCEM", iter = 1000)
-coef(AH.likan, simplify = T)
+#WHY DONT YOU WORK??
+heildarskor <- gagnarammi[!c("kyn","ald_hop", "skolstig")]
+
+
+heildarskor <- gagnarammi[c("JS_01", "SS_02", "AH_03",
+"JS_04", "SS_05", "JS_06", "SS_07", "JS_08", "SS_09", 
+"SS_10", "SS_11", "SS_12", "AH_13", "AH_14", "SS_15", 
+"JS_16", "AH_17", "TA_18", "JS_19", "TA_20", "AH_21",
+"TA_22", "TA_23", "AH_24", "AH_25", "TA_26", "TA_27",
+"TA_28", "TA_29", "TA_30", "TA_31", "AH_32")]
+
+Heild.likan <- mirt(heildarskor, itemtype = "graded")
+coef(Heild.likan, simplify = T)
 summary(Heild.likan)
 itemfit(Heild.likan)
 itemplot(Heild.likan, 3)
 itemplot(Heild.likan, 3, type = 'threshold')
 
+mirtCluster(remove = TRUE)
 
 #---- Önnur plot ---- ATH með Sigurgrím
 itemplot(JS.likan, 3, CE = TRUE)
