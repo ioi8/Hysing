92b8cd3843d7ebdce5b0b9e479b2b7b05ef6294c
ioi8 <70318969+ioi8@users.noreply.github.com>
2023-03-05 11:38:44 +0000

    Klára inngang og aðferð, setja upp MIRT líkön, leggja saman gröf

--- Undirliggjandi/MMSVerkefni201.R
+++ Undirliggjandi/MMSVerkefni201.R
@@ -12,7 +12,7 @@ tilgátur:
 - Viðhorf kennara til náms m.t.t. starfsaldur
 - Viðhorf til náms eftir landhluta (Norðvesturkjör, Norðaustur, Suðurkjördæmi, HBS)"
 
-rm(list = ls()) 
+rm(list =  ls()) 
 graphics.off() 
 cat("\014") 
 
@@ -30,29 +30,32 @@ library(tidyverse)
 gagnarammi <- read_delim("./Documents/GitHub/Megindleg.github.io/Undirliggjandi/DATA_SAL138F_IRT_Skulason.2022_Vidhorf.Til.Namsmats_v28n3404.binay.csv", 
                    delim = ";", escape_double = FALSE, trim_ws = TRUE, na = c("-999"))
 
+#-------------------Taka burt raðir með fleiri en 4 N/A gildum í spurningum-------------------------
+NAgildi <- rowSums(is.na(gagnarammi)) >= 28
+tilbuinnGogn <- gagnarammi[!NAgildi, ]
+sum(is.na(tilbuinnGogn))
+
 #----------------------------------------Breytur----------------------------------------
 #ATH AÐEINS KEYRA EINA BAKGRUNSBREYTU Í SENN!
-
 ##---------------------------------------Stærð skóla------------------------------------
 
-StorSkoli <- filter(gagnarammi, stard_sk == 51)
-litillSkoli <- filter(gagnarammi,stard_sk == 1 & 11 & 21 & 31 & 41)
+#StorSkoli <- filter(gagnarammi, stard_sk == 51)
+#litillSkoli <- filter(gagnarammi,stard_sk == 1 & 11 & 21 & 31 & 41)
 ##----------------------------------------Staðsetning------------------------------------
 HBS <- filter(gagnarammi, Ladhl == 1)
-NordVest <- filter(gagnarammi, Ladhl == 3)
-NordAust <- filter(gagnarammi, Ladhl == 6)
-Sud <- filter(gagnarammi, Ladhl == 8)
+#NordVest <- filter(gagnarammi, Ladhl == 3)
+#NordAust <- filter(gagnarammi, Ladhl == 6)
+#Sud <- filter(gagnarammi, Ladhl == 8)
 UtanHBS <- filter(gagnarammi, Hof_land == 2)
 
 ##----------------------------------------Viðhorf------------------------------------------
-VidhorfKennara <- filter(gagnarammi,hopur == 2)
-VidhorfNemenda <- filter(gagnarammi, hopur == 1)
-VidhorfNemenda <- dplyr::select(VidhorfNemenda, !c(sp_a10, sp_b05, sp_b14)) #!spr fyrir kennara
-
+VidhorStarfsfólks<- filter(gagnarammi,hopur == 2)
+VidhorfForsjá <- filter(gagnarammi, hopur == 1)
+VidhorfForsjá <- dplyr::select(gagnarammi, !c(sp_a10, sp_b05, sp_b14)) #!spr fyrir kennara
 ###----------------------------------------Starfsaldur--------------------------------------
-"11lengur" <- filter(VidhorfKennara, Strf_ald == 3)
-"6til10" <- filter(VidhorfKennara,Strf_ald == 2)
-"0til5" <- filter(VidhorfKennara,Strf_ald == 1)
+#"11lengur" <- filter(VidhorfKennara, Strf_ald == 3)
+#"6til10" <- filter(VidhorfKennara,Strf_ald == 2)
+#"0til5" <- filter(VidhorfKennara,Strf_ald == 1)
 
 
 #----------------------------------------Veljum spurningar-----------------------------------
@@ -60,7 +63,7 @@ VidhorfNemenda <- dplyr::select(VidhorfNemenda, !c(sp_a10, sp_b05, sp_b14)) #!sp
 
 
 #Settu hér inn í stað "gagnarammi" breytuheiti á viðeigandi hópi, t.d. viðhorfkennara
-hlutverk_namsmats <- dplyr::select(gagnarammi, c(sp_b01, #hve vel nemendur eru búnir undir daglegt líf og starf á fullorðinsárum
+hlutverk_namsmats1 <- dplyr::select(VidhorStarfsfólks, c(sp_b01, #hve vel nemendur eru búnir undir daglegt líf og starf á fullorðinsárum
                                                  sp_b02, #hve vel nemendur eru búnir undir nám í framhaldsskóla.
                                                  sp_b03, #hvort nemendur þurfi á sérstökum stuðningi að halda.
                                                  sp_b04, #hvort nemendur þurfi fleiri áskoranir í námi.
@@ -75,6 +78,20 @@ hlutverk_namsmats <- dplyr::select(gagnarammi, c(sp_b01, #hve vel nemendur eru b
                                                  sp_b13, #námsframvindu nemenda á sama skólaári.
                                                  sp_b14  #hve vel nemendur eru búnir undir nám á næsta stigi grunnskólans
                                                  ))
+
+hlutverk_namsmats2 <- dplyr::select(VidhorfForsjá, c(sp_b01, #hve vel nemendur eru búnir undir daglegt líf og starf á fullorðinsárum
+                                                        sp_b02, #hve vel nemendur eru búnir undir nám í framhaldsskóla.
+                                                        sp_b03, #hvort nemendur þurfi á sérstökum stuðningi að halda.
+                                                        sp_b04, #hvort nemendur þurfi fleiri áskoranir í námi.
+                                                        sp_b06, #skilning nemenda á sjálfbærni.
+                                                        sp_b07, #styrk- og veikleika nemenda við upphaf grunnskóla.
+                                                        sp_b08, #stöðu nemenda í einstökum námsgreinum.
+                                                        sp_b09, #stöðu nemenda í því námsefni sem unnið er með í kennslu hverju sinni.
+                                                        sp_b10, #stöðu nemenda miðað við jafnaldra.
+                                                        sp_b11, #stöðu nemenda út frá viðmiðum aðalnámskrár.
+                                                        sp_b12, #stöðu nemenda þegar kennari eða forsjáraðili telur þörf á.
+                                                        sp_b13, #námsframvindu nemenda á sama skólaári.
+))
 ##---------------------------------Spurningar um hlutverk námsmats------------------------------
 upplys_mat <- dplyr::select(gagnarammi, c(sp_a01, #Ég tel mikilvægt að utanaðkomandi aðili meti námsárangur nemenda til viðbótar við námsmat skólans.
                                           sp_a02, #Mér finnst að skólar ættu einir að sjá um mat á námsárangri nemenda sinna.
@@ -94,10 +111,6 @@ lykilhaefni <- dplyr::select(gagnarammi, c(sp_c01, #velferð nemenda í skóla.
                                            sp_c04  #getu nemenda til sköpunar.
                                            ))
 
-#-------------------Taka burt raðir með fleiri en 4 N/A gildum í spurningum-------------------------
-NAgildi <- rowSums(is.na(gagnarammi)) >= 4
-tilbuinnGogn <- gagnarammi[!NAgildi, ]
-sum(is.na(tilbuinnGogn))
 
 #----------------------------------------Tilreikningur----------------------------------------
 imputationgogn <- mice(tilbuinnGogn, m=3, method="pmm", maxit = 3)
@@ -106,10 +119,25 @@ sum(is.na(imputationgogn))
 describe(imputationgogn)
 
 #----------------------------------------Svarferlalíkan----------------------------------------
-Likan <- mirt(imputationgogn, itemtype = "graded")
-coef(Likan, simplify = T)
-summary(Likan)
-itemfit(Likan)
-itemplot(Likan, 3)
-itemplot(Likan, 3, type = 'threshold', )
+hlutverk_namsmats1_Likan <- mirt(hlutverk_namsmats1, itemtype = "graded")
+coef(hlutverk_namsmats1_Likan, simplify = T)
+summary(hlutverk_namsmats1_Likan)
+itemfit(hlutverk_namsmats1_Likan)
+itemplot(hlutverk_namsmats1_Likan, 3)
+itemplot(hlutverk_namsmats1_Likan, 3, type = 'threshold', )
+
+hlutverk_namsmats2_Likan <- mirt(hlutverk_namsmats2, itemtype = "graded")
+coef(hlutverk_namsmats2_Likan, simplify = T)
+summary(hlutverk_namsmats2_Likan)
+itemfit(hlutverk_namsmats2_Likan)
+itemplot(hlutverk_namsmats2_Likan, 3)
+itemplot(hlutverk_namsmats2_Likan, 3, type = 'threshold', )
+
+
+key=list(columns=1, 
+         text=list(lab=c("Kennarar"," Forráðamenn")), 
+         lines=list(lwd=1, col=c("blue","red")))
+p1 = plot(hlutverk_namsmats1_Likan,key=key, main="Viðhorf kennara")
+p2 = update(plot(hlutverk_namsmats2_Likan,col="red"))
+            p1+p2
 
