0205a07f776cb48551868e8356b6f60ee8593ec4
ioi8 <70318969+ioi8@users.noreply.github.com>
2023-03-15 15:03:05 +0000

    Uppfærsla

--- Undirliggjandi/MMSVerkefni201.R
+++ Undirliggjandi/MMSVerkefni201.R
@@ -61,37 +61,40 @@ VidhorfForsjá <- dplyr::select(gagnarammi, !c(sp_a10, sp_b13, sp_b14)) #!spr fy
 
 
 #Settu hér inn í stað "gagnarammi" breytuheiti á viðeigandi hópi, t.d. viðhorfkennara
-hlutverk_namsmats1 <- dplyr::select(VidhorStarfsfólks, c(sp_b01, #hve vel nemendur eru búnir undir daglegt líf og starf á fullorðinsárum
-                                                 sp_b02, #hve vel nemendur eru búnir undir nám í framhaldsskóla.
-                                                 sp_b03, #hvort nemendur þurfi á sérstökum stuðningi að halda.
-                                                 sp_b04, #hvort nemendur þurfi fleiri áskoranir í námi.
-                                                 sp_b05, #námsframvindu nemenda milli skólaára.
-                                                 sp_b06, #skilning nemenda á sjálfbærni.
-                                                 sp_b07, #styrk- og veikleika nemenda við upphaf grunnskóla.
-                                                 sp_b08, #stöðu nemenda í einstökum námsgreinum.
-                                                 sp_b09, #stöðu nemenda í því námsefni sem unnið er með í kennslu hverju sinni.
-                                                 sp_b10, #stöðu nemenda miðað við jafnaldra.
-                                                 sp_b11, #stöðu nemenda út frá viðmiðum aðalnámskrár.
-                                                 sp_b12, #stöðu nemenda þegar kennari eða forsjáraðili telur þörf á.
-                                                 sp_b13, #námsframvindu nemenda á sama skólaári.
-                                                 sp_b14  #hve vel nemendur eru búnir undir nám á næsta stigi grunnskólans
+hlutverk_namsmats1 <- dplyr::select(VidhorStarfsfólks, c(
+                                                sp_b01, #hve vel nemendur eru búnir undir daglegt líf og starf á fullorðinsárum
+                                                sp_b02, #hve vel nemendur eru búnir undir nám í framhaldsskóla.
+                                                sp_b03, #hvort nemendur þurfi á sérstökum stuðningi að halda.
+                                                sp_b04, #hvort nemendur þurfi fleiri áskoranir í námi.
+                                                sp_b05, #námsframvindu nemenda milli skólaára.
+                                                #sp_b06, #skilning nemenda á sjálfbærni.
+                                                sp_b07, #styrk- og veikleika nemenda við upphaf grunnskóla.
+                                                sp_b08, #stöðu nemenda í einstökum námsgreinum.
+                                                sp_b09, #stöðu nemenda í því námsefni sem unnið er með í kennslu hverju sinni.
+                                                sp_b10, #stöðu nemenda miðað við jafnaldra.
+                                                sp_b11, #stöðu nemenda út frá viðmiðum aðalnámskrár.
+                                                sp_b12, #stöðu nemenda þegar kennari eða forsjáraðili telur þörf á.
+                                                sp_b13, #námsframvindu nemenda á sama skólaári.
+                                                sp_b14  #hve vel nemendur eru búnir undir nám á næsta stigi grunnskólans
                                                  ))
 
-hlutverk_namsmats2 <- dplyr::select(VidhorfForsjá, c(sp_b01, #hve vel nemendur eru búnir undir daglegt líf og starf á fullorðinsárum
-                                                        sp_b02, #hve vel nemendur eru búnir undir nám í framhaldsskóla.
-                                                        sp_b03, #hvort nemendur þurfi á sérstökum stuðningi að halda.
-                                                        sp_b04, #hvort nemendur þurfi fleiri áskoranir í námi.
-                                                        sp_b05, #námsframvindu nemenda milli skólaára.
-                                                        sp_b06, #skilning nemenda á sjálfbærni.
-                                                        sp_b07, #styrk- og veikleika nemenda við upphaf grunnskóla.
-                                                        sp_b08, #stöðu nemenda í einstökum námsgreinum.
-                                                        sp_b09, #stöðu nemenda í því námsefni sem unnið er með í kennslu hverju sinni.
-                                                        sp_b10, #stöðu nemenda miðað við jafnaldra.
-                                                        sp_b11, #stöðu nemenda út frá viðmiðum aðalnámskrár.
-                                                        sp_b12, #stöðu nemenda þegar kennari eða forsjáraðili telur þörf á.
+hlutverk_namsmats2 <- dplyr::select(VidhorfForsjá, c(
+                                                sp_b01, #hve vel nemendur eru búnir undir daglegt líf og starf á fullorðinsárum
+                                                sp_b02, #hve vel nemendur eru búnir undir nám í framhaldsskóla.
+                                                sp_b03, #hvort nemendur þurfi á sérstökum stuðningi að halda.
+                                                sp_b04, #hvort nemendur þurfi fleiri áskoranir í námi.
+                                                sp_b05, #námsframvindu nemenda milli skólaára.
+                                                #sp_b06, #skilning nemenda á sjálfbærni. FÆRÐ Í C HLUTA
+                                                sp_b07, #styrk- og veikleika nemenda við upphaf grunnskóla.
+                                                sp_b08, #stöðu nemenda í einstökum námsgreinum.
+                                                sp_b09, #stöðu nemenda í því námsefni sem unnið er með í kennslu hverju sinni.
+                                                sp_b10, #stöðu nemenda miðað við jafnaldra.
+                                                sp_b11, #stöðu nemenda út frá viðmiðum aðalnámskrár.
+                                                sp_b12, #stöðu nemenda þegar kennari eða forsjáraðili telur þörf á.
 ))
 ##---------------------------------Spurningar um hlutverk námsmats------------------------------
-upplys_mat1 <- dplyr::select(VidhorStarfsfólks, c(sp_a01, #Ég tel mikilvægt að utanaðkomandi aðili meti námsárangur nemenda til viðbótar við námsmat skólans.
+upplys_mat1 <- dplyr::select(VidhorStarfsfólks, c(
+                                          sp_a01, #Ég tel mikilvægt að utanaðkomandi aðili meti námsárangur nemenda til viðbótar við námsmat skólans.
                                           sp_a02, #Mér finnst að skólar ættu einir að sjá um mat á námsárangri nemenda sinna.
                                           sp_a03, #Mér finnst hlutur námsmats í skólastarfi of mikill.
                                           sp_a04, #Mér finnst námsmat nauðsynlegur hluti skólastarfs.
@@ -102,48 +105,53 @@ upplys_mat1 <- dplyr::select(VidhorStarfsfólks, c(sp_a01, #Ég tel mikilvægt a
                                           sp_a09, #Mikilvægt er að til séu upplýsingar um stöðu skólakerfisins almennt.
                                           sp_a10  #Ég á auðvelt með að meta frammistöðu nemenda samkvæmt viðmiðum aðalnámskrár.
                                           ))
-upplys_mat2 <- dplyr::select(VidhorfForsjá, c(sp_a01, #Ég tel mikilvægt að utanaðkomandi aðili meti námsárangur nemenda til viðbótar við námsmat skólans.
-                                           sp_a02, #Mér finnst að skólar ættu einir að sjá um mat á námsárangri nemenda sinna.
-                                           sp_a03, #Mér finnst hlutur námsmats í skólastarfi of mikill.
-                                           sp_a04, #Mér finnst námsmat nauðsynlegur hluti skólastarfs.
-                                           sp_a05, #Mikilvægt er að framhaldsskólar sem það kjósa geti notað eigin próf við inntöku nýnema.
-                                           sp_a06, #Mikilvægt er að kennarar í öllum skólum landsins hafi aðgang að sömu prófum og matstækjum.
-                                           sp_a07, #Mikilvægt er að til séu upplýsingar um stöðu einstakra skóla.
-                                           sp_a08, #Mikilvægt er að til séu upplýsingar um stöðu skóla í einstökum landshlutum eða sveitarfélögum.
-                                           sp_a09, #Mikilvægt er að til séu upplýsingar um stöðu skólakerfisins almennt.
+upplys_mat2 <- dplyr::select(VidhorfForsjá, c(
+                                          sp_a01, #Ég tel mikilvægt að utanaðkomandi aðili meti námsárangur nemenda til viðbótar við námsmat skólans.
+                                          sp_a02, #Mér finnst að skólar ættu einir að sjá um mat á námsárangri nemenda sinna.
+                                          sp_a03, #Mér finnst hlutur námsmats í skólastarfi of mikill.
+                                          sp_a04, #Mér finnst námsmat nauðsynlegur hluti skólastarfs.
+                                          sp_a05, #Mikilvægt er að framhaldsskólar sem það kjósa geti notað eigin próf við inntöku nýnema.
+                                          sp_a06, #Mikilvægt er að kennarar í öllum skólum landsins hafi aðgang að sömu prófum og matstækjum.
+                                          sp_a07, #Mikilvægt er að til séu upplýsingar um stöðu einstakra skóla.
+                                          sp_a08, #Mikilvægt er að til séu upplýsingar um stöðu skóla í einstökum landshlutum eða sveitarfélögum.
+                                          sp_a09, #Mikilvægt er að til séu upplýsingar um stöðu skólakerfisins almennt.
 ))
 ##-----------------------------Spurningar um  mat tengt lykilhæfni manntunar í aðalnámsskrá-------
-lykilhaefni1 <- dplyr::select(VidhorStarfsfólks, c(sp_c01, #velferð nemenda í skóla.
-                                           sp_c02, #vinnubrögð nemenda og námstækni.
-                                           sp_c03, #virkni nemenda í lýðræði og jafnrétti.
-                                           sp_c04  #getu nemenda til sköpunar.
+lykilhaefni1 <- dplyr::select(VidhorStarfsfólks, c(
+                                          sp_c01, #velferð nemenda í skóla.
+                                          sp_c02, #vinnubrögð nemenda og námstækni.
+                                          sp_c03, #virkni nemenda í lýðræði og jafnrétti.
+                                          sp_c04,  #getu nemenda til sköpunar.
+                                          sp_b06, #skilning nemenda á sjálfbærni.
                                            ))
-lykilhaefni2 <- dplyr::select(VidhorfForsjá, c(sp_c01, #velferð nemenda í skóla.
-                                            sp_c02, #vinnubrögð nemenda og námstækni.
-                                            sp_c03, #virkni nemenda í lýðræði og jafnrétti.
-                                            sp_c04  #getu nemenda til sköpunar.
+lykilhaefni2 <- dplyr::select(VidhorfForsjá, c(
+                                          sp_c01, #velferð nemenda í skóla.
+                                          sp_c02, #vinnubrögð nemenda og námstækni.
+                                          sp_c03, #virkni nemenda í lýðræði og jafnrétti.
+                                          sp_c04,  #getu nemenda til sköpunar.
+                                          sp_b06, #skilning nemenda á sjálfbærni.
 ))
 
 
-#----------------------------------------Svarferlalíkan----------------------------------------
+#----------------------------------------Svarferlalíkan hlutverk----------------------------------------
 set.seed(7)
 
 hlutverk_namsmats1 <- mice(hlutverk_namsmats1, m=3, method="pmm", maxit = 3) #Tilreikningur
 hlutverk_namsmats1 <- complete(hlutverk_namsmats1, 3)
-hlutverk_namsmats1_Likan <- mirt(hlutverk_namsmats1, itemtype = "graded")
+hlutverk_namsmats1_Likan <- mirt(hlutverk_namsmats1, itemtype = "2PL", )
 coef(hlutverk_namsmats1_Likan, simplify = T)
 summary(hlutverk_namsmats1_Likan)
 itemfit(hlutverk_namsmats1_Likan)
-itemplot(hlutverk_namsmats1_Likan, 3)
-itemplot(hlutverk_namsmats1_Likan, 3, type = 'threshold', )
+itemplot(hlutverk_namsmats1_Likan, 1)
+itemplot(hlutverk_namsmats1_Likan, 1, type = 'threshold', )
 
 hlutverk_namsmats2 <- mice(hlutverk_namsmats2, m=3, method="pmm", maxit = 3) #Tilreikningur
 hlutverk_namsmats2 <- complete(hlutverk_namsmats2, 3)
-hlutverk_namsmats2_Likan <- mirt(hlutverk_namsmats2, itemtype = "graded")
+hlutverk_namsmats2_Likan <- mirt(hlutverk_namsmats2, itemtype = "2PL")
 coef(hlutverk_namsmats2_Likan, simplify = T)
 summary(hlutverk_namsmats2_Likan)
 itemfit(hlutverk_namsmats2_Likan)
-itemplot(hlutverk_namsmats2_Likan, 3)
+itemplot(hlutverk_namsmats2_Likan, 1)
 itemplot(hlutverk_namsmats2_Likan, 3, type = 'threshold', )
 
 
@@ -155,24 +163,25 @@ HN1 + HN2
 M2(hlutverk_namsmats1_Likan)
 M2(hlutverk_namsmats2_Likan)
 
-#----------------------------------------Svarferlalíkan----------------------------------------
+
+#----------------------------------------Svarferlalíkan Upplysingar----------------------------------------
 upplys_mat1 <- mice(upplys_mat1, m=3, method="pmm", maxit = 3) #Tilreikningur
 upplys_mat1 <- complete(upplys_mat1, 3)
-upplys_mat1_Likan <- mirt(upplys_mat1, itemtype = "graded")
+upplys_mat1_Likan <- mirt(upplys_mat1, itemtype = "2PL")
 coef(upplys_mat1_Likan, simplify = T)
 summary(upplys_mat1_Likan)
 itemfit(upplys_mat1_Likan)
-itemplot(upplys_mat1_Likan, 3)
-itemplot(upplys_mat1_Likan, 3, type = 'threshold', )
+itemplot(upplys_mat1_Likan, 2)
+itemplot(upplys_mat1_Likan, 2, type = 'threshold', )
 
 upplys_mat2 <- mice(upplys_mat2, m=3, method="pmm", maxit = 3) #Tilreikningur
 upplys_mat2 <- complete(upplys_mat2, 3)
-upplys_mat2_Likan <- mirt(upplys_mat2, itemtype = "graded")
+upplys_mat2_Likan <- mirt(upplys_mat2, itemtype = "2PL")
 coef(upplys_mat2_Likan, simplify = T)
 summary(upplys_mat2_Likan)
 itemfit(upplys_mat2_Likan)
-itemplot(upplys_mat2_Likan, 3)
-itemplot(upplys_mat2_Likan, 3, type = 'threshold', )
+itemplot(upplys_mat2_Likan, 2)
+itemplot(upplys_mat2_Likan, 2, type = 'threshold', )
 
 key=list(columns=2,text=list(lab=c("Kennarar"," Forráðamenn")), lines=list(lwd=2, col=c("blue","red")))
 HN1 = plot(upplys_mat1_Likan,key=key, main="Viðhorf á mikilvægi upplýsinga og um framkvæmd mats í skólum")
@@ -182,11 +191,11 @@ HN1+HN2
 M2(upplys_mat1_Likan)
 M2(upplys_mat2_Likan)
 
-#----------------------------------------Svarferlalíkan----------------------------------------
+#----------------------------------------Svarferlalíkan Lykilhæfni----------------------------------------
 
 lykilhaefni1 <- mice(lykilhaefni1, m=3, method="pmm", maxit = 3) #Tilreikningur
 lykilhaefni1 <- complete(lykilhaefni1, 3)
-lykilhaefni1_likan <- mirt(lykilhaefni1, itemtype = "graded")
+lykilhaefni1_likan <- mirt(lykilhaefni1, itemtype = "2PL")
 coef(lykilhaefni1_likan, simplify = T)
 summary(lykilhaefni1_likan)
 itemfit(lykilhaefni1_likan)
@@ -195,7 +204,7 @@ itemplot(lykilhaefni1_likan, 3, type = 'threshold', )
 
 lykilhaefni2 <- mice(lykilhaefni2, m=3, method="pmm", maxit = 3) #Tilreikningur
 lykilhaefni2 <- complete(lykilhaefni2, 3)
-lykilhaefni2_likan <- mirt(lykilhaefni2, itemtype = "graded")
+lykilhaefni2_likan <- mirt(lykilhaefni2, itemtype = "2PL")
 coef(lykilhaefni2_likan, simplify = T)
 summary(lykilhaefni2_likan,fit.measures = TRUE)
 itemfit(lykilhaefni2_likan)
@@ -212,3 +221,4 @@ M2(lykilhaefni2_likan)
 M2(lykilhaefni1_likan)
 
 #Finna summutölu
+
