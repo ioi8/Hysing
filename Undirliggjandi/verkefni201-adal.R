bf56b5900376274a6ff391e67c37f5094bf6bcbd
AdalheidurM <adm7@hi.is>
2023-03-20 10:22:48 +0000

    Uppfæra

--- Undirliggjandi/verkefni201-adal.R
+++ Undirliggjandi/verkefni201-adal.R
@@ -229,8 +229,6 @@ legend(x='top', c('Forsjáraðilar', 'Starfsfólk skóla'), fill=c('blue', 'red'
        border="black", box.lty = 0, bty = "n", ncol=2, inset=-0.15, cex=1)
 
 
-
-
 # Mat á stikum líkans 
 
 # Leifar líkans og kí-kvaðratpróf
